3675f5622aefcc34c3c3cd3e4640cad1bd01ef44
ForgivenCheese <119278402+ForgivenCheese@users.noreply.github.com>
2023-07-12 22:40:35 +0800

    Update Login.py
    completed password complexity (Mark)

--- Login.py
+++ Login.py
@@ -3,10 +3,10 @@ from flask_mysqldb import MySQL
 import MySQLdb.cursors
 from flask_bcrypt import Bcrypt
 import re
-import cryptography
 from cryptography.fernet import Fernet
 from SQL_Functions import *
 from forms import *
+
 app = Flask(__name__)
 bcrypt = Bcrypt()
 
@@ -23,41 +23,78 @@ app.config['MYSQL_DB'] = 'sys_sec'
 mysql = MySQL(app)
 
 
-# http://localhost:5000/P_SQL/ - this will be the login page, we need to use both GET and POST requests
+def password_check(password):
+    """
+    Verify the strength of 'password'
+    Returns a dict indicating the wrong criteria
+    A password is considered strong if:
+        8 characters length or more
+        1 digit or more
+        1 symbol or more
+        1 uppercase letter or more
+        1 lowercase letter or more
+    """
+
+    # calculating the length
+    length_error = len(password) < 8
+
+    # searching for digits
+    digit_error = re.search(r"\d", password) is None
+
+    # searching for uppercase
+    uppercase_error = re.search(r"[A-Z]", password) is None
+
+    # searching for lowercase
+    lowercase_error = re.search(r"[a-z]", password) is None
+
+    # searching for symbols
+    symbol_error = re.search(r"[ !#$%&'()*+,-./[\]^`{|}~"+r'"]', password) is None
+
+    # overall result
+    password_ok = not (length_error or digit_error or uppercase_error or lowercase_error or symbol_error)
+
+    return {
+        'password_ok': password_ok,
+        'length_error': length_error,
+        'digit_error': digit_error,
+        'uppercase_error': uppercase_error,
+        'lowercase_error': lowercase_error,
+        'symbol_error': symbol_error,
+    }
+
+
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
     return redirect(url_for('login'))
 
 
-# c = Counter()
-
-#limiting done
 @app.route('/WebApp', methods=['GET', 'POST'])
 def login():
-    msg=''
+    msg = ''
     login = LoginForm()
     if login.validate_on_submit():
         username = login.username.data
         password = login.password.data
 
-        if SQL_Login(username, password) == 0:
+        # Validate the password using password_check()
+        password_validation = password_check(password)
+
+        if SQL_Login(username, password) == 0 and password_validation['password_ok']:
             return redirect(url_for('home'))
-        elif SQL_Login(username, password) == 1:
+        else:
             msg = 'Incorrect Username/Password'
-            return render_template('index.html', msg=msg, form=login)
+
+            # Pass the password validation results to the template
+            return render_template('index.html', msg=msg, form=login, password_validation=password_validation)
 
     return render_template('index.html', msg='', form=login)
 
 
-# http://localhost:5000/P_SQL/logout - the logout page
 @app.route('/logout')
 def logout():
-    # Remove session data, the user will be logged out
     session.pop('loggedin', None)
     session.pop('id', None)
     session.pop('username', None)
-
-    # Redirect to login page
     return redirect(url_for('login'))
 
 
@@ -65,39 +102,38 @@ def logout():
 def register():
     msg = ''
     regform = RegisterForm()
-
     if regform.validate_on_submit():
         username = regform.username.data
         password = regform.password.data
         email = regform.email.data
-        if SQL_Register(username, password, email) == 0:
-            msg = 'Successful registration'
-        elif SQL_Register(username, password, email) == 1:
-            msg = 'Duplicate found'
+
+        # Validate the password using password_check()
+        password_validation = password_check(password)
+
+        if password_validation['password_ok']:
+            if SQL_Register(username, password, email) == 0:
+                msg = 'Successful registration'
+            elif SQL_Register(username, password, email) == 1:
+                msg = 'Duplicate found'
+        else:
+            msg = 'Invalid password. Please make sure your password meets the requirements.'
+
     elif request.method == 'POST':
-        # Form is empty (no POST Data)
         msg = 'Please fill out the form'
 
-    return render_template('register.html',msg=msg, form=regform)
+    return render_template('register.html', msg=msg, form=regform)
 
 
-
-# http://localhost:5000/home - Home page, only accessible for logged in users
 @app.route('/home')
 def home():
-    # Check if user is logged in
     if 'loggedin' in session:
         return render_template('home.html', username=session['username'])
-
-    # User is not loggedin, redirect to login page
     return redirect(url_for('login'))
 
 
-# http://localhost:5000/profile = Profile page, only accesible for logged in users
 @app.route('/profile')
 def profile():
     if 'loggedin' in session:
-        # All account info for the user to display it on the profile page
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute("SELECT * FROM users WHERE id = %s", (session['id'],))
         account = cursor.fetchone()
@@ -110,15 +146,13 @@ def profile():
         f = Fernet(key)
         decrypted_email = f.decrypt(encrypted_email)
 
-        # Show profile page with account info
         return render_template('profile.html', account=account, decrypted_email=decrypted_email)
-
-    # User is not logged in, redirect to login page
     return redirect(url_for('login'))
+
 
 @app.route('/card', methods=['GET', 'POST'])
 def card():
-    msg=''
+    msg = ''
     regcard = RegisterCard()
     nList = readCards()
     print(nList)
@@ -129,15 +163,12 @@ def card():
         card_num = regcard.card_num.data
         exp_date = regcard.exp_date.data
         cvv = regcard.cvv.data
-        if SQL_registerCard(card_num,fname,lname,exp_date, cvv) == 0:
+        if SQL_registerCard(card_num, fname, lname, exp_date, cvv) == 0:
             msg = 'Card added'
-        elif SQL_registerCard(card_num,fname,lname,exp_date,cvv) == 1:
+        elif SQL_registerCard(card_num, fname, lname, exp_date, cvv) == 1:
             msg = "Error in adding card"
-
     else:
-        print(regcard.fname.data,regcard.lname.data,regcard.card_num.data,regcard.exp_date.data,
-              regcard.cvv.data)
-
+        print(regcard.fname.data, regcard.lname.data, regcard.card_num.data, regcard.exp_date.data, regcard.cvv.data)
     return render_template('registercard.html', msg=msg, form=regcard, cards=nList)
 
 
