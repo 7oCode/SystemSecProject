3dc1efbe0cc3fe0fca8a7ffc43b21247abe1ab8d
Olfsen <220274X@mymail.nyp.edu.sg>
2023-08-14 00:01:33 +0800

    RBAC reset password in progress

--- Login1.py
+++ Login1.py
@@ -56,7 +56,7 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=0.5)
 # Enter database connection details
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'marksql'
+app.config['MYSQL_PASSWORD'] = 'password123'
 app.config['MYSQL_DB'] = 'sys_sec'
 
 app.config["MAIL_SERVER"] = 'smtp.gmail.com'
